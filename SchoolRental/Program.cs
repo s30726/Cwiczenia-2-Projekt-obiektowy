f9695d1a3a72955f5d0d22a26f87d968a8f6310e
karolkubica <s30726@pjwstk.edu.pl>
2026-03-25 14:54:25 +0100

    Add initial project files

---
+++ SchoolRental/Program.cs
@@ -0,0 +1,18 @@
+﻿using SchoolRental.Models;
+using SchoolRental.Models.Equipment;
+using SchoolRental.Models.Users;
+
+namespace SchoolRental;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Device myLaptop = new Laptop("Asus", 8, "i7");
+        Console.Write(myLaptop);
+        User myself = new Student("Karol", "Kubica", "s30726");
+        Console.Write(myself);
+        Rental myFirstRent = new Rental(myself, myLaptop, 5);
+        Console.Write(myFirstRent);
+    }
+}
