7983b65ffb4238319e71fae903edb60a203526e4
karolkubica <s30726@pjwstk.edu.pl>
2026-03-25 18:21:20 +0100

    Zmiana statusu została przeniesiona do klasy Device

--- SchoolRental/Models/Equipment/Device.cs
+++ SchoolRental/Models/Equipment/Device.cs
@@ -4,13 +4,16 @@ public class Device
 {
     public Guid Id { get; private set; }
     public string Name { get; set; }
-    public DeviceStatus Status { get; set; }
+    public DeviceStatus Status { get; private set; }
+    public void MarkAsRented() => Status = DeviceStatus.Rented;
+    public void MarkAsAvailable() => Status = DeviceStatus.Available;
+    public void MarkAsUnavailable() => Status = DeviceStatus.Unavailable;
 
     protected Device(string name)
     {
         Id = Guid.NewGuid();
         Name = name;
-        Status = DeviceStatus.Available;
+        MarkAsUnavailable();
     }
 
     public override string ToString()
--- SchoolRental/Models/Rental.cs
+++ SchoolRental/Models/Rental.cs
@@ -1,40 +1,41 @@
 ﻿using SchoolRental.Models.Users;
-using  SchoolRental.Models.Equipment;
+using SchoolRental.Models.Equipment;
 
 namespace SchoolRental.Models;
 
 public class Rental
 {
-public Guid Id { get; private set; }
-public User User { get; private set; }
-public Device Device { get; private set; }
+    public Guid Id { get; private set; }
+    public User User { get; private set; }
+    public Device Device { get; private set; }
 
-public DateTime RentalDate { get; private set; }
-public DateTime? ReturnDate { get; private set; }
-public DateTime RentalEndDate { get; private set; }
+    public DateTime RentalDate { get; private set; }
+    public DateTime? ReturnDate { get; private set; }
+    public DateTime RentalEndDate { get; private set; }
 
-public decimal Penalty { get; private set; }
+    public decimal Penalty { get; private set; }
 
-public bool IsReturned => ReturnDate.HasValue;
-public bool IsOverdue => !IsReturned && RentalEndDate < DateTime.Now;
+    public bool IsReturned => ReturnDate.HasValue;
+    public bool IsOverdue => !IsReturned && RentalEndDate < DateTime.Now;
 
-public Rental(User user, Device device, int rentalDays)
-{
-    Id = Guid.NewGuid();
-    User = user;
-    Device = device;
-    RentalDate = DateTime.Now;
-    RentalEndDate = RentalDate.AddDays(rentalDays);
-}
+    public Rental(User user, Device device, int rentalDays)
+    {
+        Id = Guid.NewGuid();
+        User = user;
+        Device = device;
+        RentalDate = DateTime.Now;
+        RentalEndDate = RentalDate.AddDays(rentalDays);
+    }
 
-public void Return(decimal penalty)
-{
-    ReturnDate = DateTime.Now;
-    Penalty = penalty;
-}
+    public void Return(decimal penalty)
+    {
+        ReturnDate = DateTime.Now;
+        Penalty = penalty;
+    }
 
-public override string ToString()
-{
-    return $"Id: {Id} | User: {User.FirstName} {User.LastName} | Device:  {Device.Name} | Due: {RentalEndDate} | Returned: {ReturnDate} | Penalty: {Penalty}\n";
-}
+    public override string ToString()
+    {
+        return
+            $"Id: {Id} | User: {User.FirstName} {User.LastName} | Device:  {Device.Name} | Due: {RentalEndDate} | Returned: {ReturnDate} | Penalty: {Penalty}\n";
+    }
 }
